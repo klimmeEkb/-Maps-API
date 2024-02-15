6155f11b9461d8732593840089c6eeb9f0ded3ff
klimmeEkb <151643633+klimmeEkb@users.noreply.github.com>
2024-02-15 22:01:49 +0500

    Working API.py
    it's work

--- API.py
+++ API.py
@@ -4,36 +4,39 @@ import requests
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
+from PyQt5.QtCore import Qt
+import pathlib
+import os
+import os.path
 
-class Example(QWidget):
+
+class MapFounderApi(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi("design.ui", self)
-        #self.button.clicked.connect(self.map_finding)
+        uic.loadUi(os.path.abspath("design.ui"), self)
+        self.button.clicked.connect(self.map_finding)
         self.initUI()
 
     def getImage(self, coords1, coords2):
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll={coord1},{coords2}&spn=0.002,0.002&l=map"
-        response = requests.get(map_request)
-        if not response:
-            print("Ошибка выполнения запроса:")
-            print(map_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
-            sys.exit(1)
-        else:
+        if (float(coords1) < 86 and float(coords2) < 86):
+            map_request = f"https://static-maps.yandex.ru/1.x/?ll={coords1},{coords2}&spn=0.01,0.05&l=map"
+            response = requests.get(map_request)
+            if not response:
+                print("Ошибка выполнения запроса:")
+                print(map_request)
+                print("Http статус:", response.status_code, "(", response.reason, ")")
+                sys.exit(1)    
+            self.map_file = os.path.abspath("map.png")
+            with open(self.map_file, "wb") as file:
+                file.write(response.content)
             self.pixmap = QPixmap(self.map_file)
-            self.image = QLabel(self)
-            self.image.move(120, 330)
-            self.image.resize(460, 460)
             self.image.setPixmap(self.pixmap)
-
-        # Запишем полученное изображение в файл.
-        self.map_file = "map.png"
-        with open(self.map_file, "wb") as file:
-            file.write(response.content)
 
     def initUI(self):
         self.setWindowTitle('Большая задача по Maps API')
+        self.image = QLabel(self)
+        self.image.move(60, 330)
+        self.image.resize(460, 460)
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Space:
@@ -42,13 +45,13 @@ class Example(QWidget):
     def map_finding(self):
         self.getImage(self.coords1.text(), self.coords2.text())
 
-    def close(self, event):
+    def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = MapFounderApi()
     ex.show()
     sys.exit(app.exec())
