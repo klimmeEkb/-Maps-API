637fadde5a312e1f4abd09c1828e00b5b486f707
klimmeEkb <151643633+klimmeEkb@users.noreply.github.com>
2024-02-16 19:07:58 +0500

    1.1 API.py
    you can press PgUp or PgDown and see a good thing

--- API.py
+++ API.py
@@ -14,12 +14,13 @@ class MapFounderApi(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi(os.path.abspath("design.ui"), self)
+        self.i = 0.0
         self.button.clicked.connect(self.map_finding)
         self.initUI()
 
-    def getImage(self, coords1, coords2, mash):
-        if (float(coords1) < 86 and float(coords2) < 86 and float(mash) <= 90):
-            map_request = f"https://static-maps.yandex.ru/1.x/?ll={coords1},{coords2}&spn=0.01,{mash}&l=map"
+    def getImage(self, coords1, coords2, mash, i):
+        if (float(coords1) < 86 and float(coords2) < 86 and float(mash) <= 90 and float(i) + float(mash) >= 0):
+            map_request = f"https://static-maps.yandex.ru/1.x/?ll={coords1},{coords2}&spn=0.01,{str(float(mash) + float(i))}&l=map"
             response = requests.get(map_request)
             if not response:
                 print("Ошибка выполнения запроса:")
@@ -41,9 +42,16 @@ class MapFounderApi(QWidget):
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Space or event.key() == Qt.Key_Enter:
             self.map_finding()
+            self.i = 0
+        if event.key() == Qt.Key_PageUp:
+            self.i -= 0.5
+            self.map_finding()
+        if event.key() == Qt.Key_PageDown:
+            self.i += 0.5
+            self.map_finding()
 
     def map_finding(self):
-        self.getImage(self.coords1.text(), self.coords2.text(), self.mash.text())
+        self.getImage(self.coords1.text().strip(), self.coords2.text().strip(), self.mash.text().strip(), self.i)
 
     def closeEvent(self, event):
         os.remove(self.map_file)
